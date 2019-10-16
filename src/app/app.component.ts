9405ecd1c87a3298a6b71a99b0ec64419ba98d2c
MATEUS PERREIRA SATIRO REYES <a9261456@rn.senac.br>
2019-10-16 19:52:03 -0300

    criado o projeto do angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { OpmService } from './opm.service';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+
+
+
+class Opm{
+  id: number;
+  nome: string;
+  sigla: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'sisdaf-front';
+export class AppComponent implements OnInit{
+
+opm = new Opm();
+opms = [];
+
+constructor(private service: OpmService){}
+
+ngOnInit(): void {
+  this.buscaOpm();
+
+}
+
+buscaOpm(){
+  this.service.buscaTodos().then(dados =>{
+
+    this.opms = dados
+
+  });
+}
+
+salvar(formOpm: NgForm){
+
+  this.opm.nome = formOpm.value.nome;
+  this.opm.sigla = formOpm.value.sigla;
+
+  this.service.salvaOpm(this.opm).then(o => {
+
+    this.buscaOpm();
+  });
+
+  this.opm = new Opm();
+
+}
+
+
 }
---
+++ src/app/opm.service.ts
@@ -0,0 +1,28 @@
+import { environment } from './../environments/environment';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable()
+export class OpmService{
+
+private readonly URL_BUSCA_TODOS =`${environment.apiUrl}/opms`;
+private readonly URL_SALVA_OPM = `${environment.apiUrl}/opms`;
+
+constructor(private http: HttpClient){}
+
+//busca todas as opms
+
+  buscaTodos(): Promise<any>{
+    return this.http.get(this.URL_BUSCA_TODOS)
+    .toPromise();
+
+  }
+
+  salvaOpm(opm: any):Promise<any>{
+    return this.http.post(this.URL_SALVA_OPM, opm).toPromise();
+  }
+
+}
+
+
