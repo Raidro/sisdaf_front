462184fd6fe25064dbb07648a9a79fb5aec72fad
Raidro <mateus_sat@hotmail.com>
2019-10-17 20:32:33 -0300

    criado o module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { OpmModule } from './opm/opm.module';
@@ -13,8 +11,6 @@ import { OpmModule } from './opm/opm.module';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpClientModule,
     OpmModule
   ],
   providers: [],
